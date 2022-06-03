af2be368d696644b64a06d9550fb37be8cf3cf48
Alex <alexcu21@live.com>
2022-06-02 18:48:06 -0600

    fixing index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import Template from '@templates/Template.js';
 import '@styles/main.scss';
 
 (async function App() {
-  const main = null || document.getElementById('main');
+  const main = document.getElementById('main');
   main.innerHTML = await Template();
 })();
